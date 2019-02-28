9c80f851d9c80e7ac2db58223e7bb3c4e6d91089
huangyan <shhuangyan1@163.com>
2019-02-28 14:26:19 +0800

    init

---
+++ application/api/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/2/28
+ * Time: 14:16
+ */
+namespace app\api\controller;
+
+class Index{
+    public function index()
+    {
+    echo 'hello world';
+    }
+}
